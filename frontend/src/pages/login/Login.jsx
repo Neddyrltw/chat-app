b17e4b5568617f9ec80d114154fb32b4758999f6
Nicholas Eddy <nicholas.g.eddy@gmail.com>
2024-10-23 21:58:44 -0400

    Login page buildout  complete

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+            <div className='w-full p-6 rounded-lg shadow-md bg-gray-600 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30'>
+                <h1 className='text-3xl font-semibold text-center text-grey-300'>Login
+                    <span className='text-amber-950'> CosmicConnect</span>
+                </h1>
+    
+                <form>
+                    <div>
+                        <label className='label p-2'>
+                            <span className='text-base label-text'>Username</span>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder='Enter Username'
+                            className='w-full input input-bordered h-10' />
+                    </div>
+                    <div>
+                    <label className='label'>
+                            <span className='text-base label-text'>Password</span>
+                        </label>
+                        <input
+                            type="password"
+                            placeholder='Enter Password'
+                            className='w-full input input-bordered h-10' />
+                    </div>
+                    <a href="#" className='text-sm hover:underline hover:text-sky-400 mt-2 inline-block'>
+                        Don't have an account?
+                    </a>
+    
+                    <div>
+                        <button className='btn btn-block bg-amber-950 btn-sm mt-2'>Login</button>
+                    </div>
+                </form>
+    
+            </div>
+          
+        </div>
+      )
+};
+
+export default Login;
+
+
+// STARTER CODE FOR PAGE
+// import React from 'react';
+
+// const Login = () => {
+//     return (
+//         <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+//             <div className='w-full p-6 rounded-lg shadow-md bg-gray-600 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30'>
+//                 <h1 className='text-3xl font-semibold text-center text-grey-300'>Login
+//                     <span className='text-amber-950'> CosmicConnect</span>
+//                 </h1>
+    
+//                 <form>
+//                     <div>
+//                         <label className='label p-2'>
+//                             <span className='text-base label-text'>Username</span>
+//                         </label>
+//                         <input
+//                             type="text"
+//                             placeholder='Enter Username'
+//                             className='w-full input input-bordered h-10' />
+//                     </div>
+//                     <div>
+//                     <label className='label'>
+//                             <span className='text-base label-text'>Password</span>
+//                         </label>
+//                         <input
+//                             type="password"
+//                             placeholder='Enter Password'
+//                             className='w-full input input-bordered h-10' />
+//                     </div>
+//                     <a href="#" className='text-sm hover:underline hover:text-sky-400 mt-2 inline-block'>
+//                         Don't have an account?
+//                     </a>
+    
+//                     <div>
+//                         <button className='btn btn-block bg-amber-950 btn-sm mt-2'>Login</button>
+//                     </div>
+//                 </form>
+    
+//             </div>
+          
+//         </div>
+//       )
+// };
+
+// export default Login;
