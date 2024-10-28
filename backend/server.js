5c204996aaeb5bdd73b17f63e5c388ba901211c8
Nicholas Eddy <nicholas.g.eddy@gmail.com>
2024-10-27 20:59:38 -0400

    deployment setup

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -12,7 +13,9 @@ import {app, server} from "./socket/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
+
+const __dirname = path.resolve();
 
 app.use(express.json()); // parse incoming requests with JSON payloads (from req.body)
 app.use(cookieParser()); // parse incoming cookies from req.cookies
@@ -20,6 +23,12 @@ app.use(cookieParser()); // parse incoming cookies from req.cookies
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 
